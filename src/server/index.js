3faa6b6cd56960143d70b7976075d617d9ec390c
Seth Lowie <slowie@exzeo.com>
2016-04-26 16:25:38 -0400

    serve static files

--- src/server/index.js
+++ src/server/index.js
@@ -3,9 +3,7 @@ import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('hello from aws');
-})
+app.use(express.static(`${__dirname}/../../build/`));
 
 app.listen(PORT, (err) => {
   if (err) throw err;
